ef15f7d9a3c50eaa0d1fba7b370e578f81daf938
MokeEire <markkbarrett@gmail.com>
2021-03-02 18:11:55 -0800

    Rework the theme function to specify a theme object and build onto it

--- theme_mark.R
+++ theme_mark.R
@@ -56,82 +56,117 @@ theme_mark = function(title_family = "Cabin Condensed",
   line_colour = colour_pal[3]
   grid_colour = colour_pal[4]
   
+  # plots_pane == True & md == False
+  theme_obj = ggplot2::theme_minimal(base_size = if_else(plots_pane, base_size, 11)) +
+    ggplot2::theme(
+      # Main elements
+      text = element_text(family = text_family,
+                          colour = text_colour),
+      title = element_text(family = title_family,
+                           colour = text_colour, 
+                           margin = margin(b = 5)),
+      
+      plot.title = element_text(family = title_family,
+                                face = "bold",
+                                lineheight = 1.2,
+                                hjust = 0),
+      plot.subtitle = element_text(lineheight = 1,
+                                   family = subtitle_family,
+                                   hjust = 0),
+      
+
+      line = element_line(colour = line_colour),
+      panel.grid.major = element_line(colour = grid_colour),
+      panel.grid.minor = element_blank(),
+      
+      # Plot titles
+      plot.title.position = "plot",
+      
+      # Plot Margin & Caption
+      plot.margin = plot_margin,
+      plot.caption.position = "plot", 
+      plot.caption = element_text(hjust = 0,
+                                  colour = line_colour, 
+                                  margin = margin(t = 25, b = 25, l = 0, r = 0)),
+      
+      
+      # Background
+      plot.background = element_rect(fill = bg_colour,
+                                     colour = bg_colour),
+      
+      # Axes
+      axis.text = element_text(colour = text_colour,
+                               margin = margin(t = 25, b = 25, l = 25, r = 25)),
+      
+      axis.line = element_line(colour = line_colour),
+      axis.title = element_text(hjust = 1,
+                                face = "italic",
+                                margin = margin(t = 10, b = 10, l = 10, r = 10)),
+      
+
+      axis.title.y.left = element_text(hjust = 1,
+                                       face = "italic",
+                                       margin = margin(t = 15, r = 10, l = 10)),
+      
+
+      
+      axis.title.y.right = element_text(hjust = 0, 
+                                        face = "italic",
+                                        margin = margin(t = 15, r = 10, l = 10)),
+      
+      
+      # Facets
+      strip.text.y.left = element_text(angle = 0, 
+                                       vjust = 0.5, 
+                                       hjust = 1),
+      strip.placement = "outside",
+      strip.text.y.right = element_text(angle = 0, 
+                                        vjust = 0.5, 
+                                        hjust = 0)
+      
+    )
+  
   if (plots_pane == FALSE & md == FALSE) {
-    ggplot2::theme_minimal() +
+    theme_obj +
       ggplot2::theme(
-        # Main elements
-        text = element_text(family = text_family,
-                            size = base_size,
-                            colour = text_colour),
-        title = element_text(family = title_family,
-                             colour = text_colour),
-        line = element_line(colour = line_colour),
-        panel.grid.major = element_line(colour = grid_colour),
-        panel.grid.minor = element_blank(),
-        
+        text = element_text(size = base_size),
         # Plot titles
-        plot.title = element_text(face = "bold",
-                                  size = base_size * 2,
-                                  lineheight = 1.2),
-        plot.title.position = "plot",
-        plot.subtitle = element_text(size = base_size * 1.7,
-                                     lineheight = 1.2),
-        # Plot Margin & Caption
-        plot.margin = plot_margin,
-        plot.caption.position = "plot", 
-        plot.caption = element_text(hjust = 0, 
-                                    size = base_size),
-        # Background
-        plot.background = element_rect(fill = bg_colour,
-                                       colour = bg_colour),
+        plot.title = element_text(size = base_size * 2),
+        plot.subtitle = element_text(size = base_size * 1.4),
+        plot.caption = element_text(size = base_size),
         # Axes
-        axis.text = element_text(size = base_size * 1.25, colour = text_colour),
-        axis.title = element_text(size = base_size * 1.6,
-                                  hjust = 1,
-                                  face = "italic"),
-        axis.title.y.left = element_text(margin = margin(r = 15)),
-        axis.title.y.right = element_text(hjust = 0, margin = margin(l = 15)),
-        axis.line = element_line(colour = line_colour),
+        axis.text = element_text(size = base_size * 1.25),
+        axis.title = element_text(size = base_size * 1.6),
         # Legend
         legend.title = element_text(size = base_size * 1.3),
         legend.text = element_text(size = base_size * 1.1)
+        
+        
       )
   } else if (plots_pane == FALSE & md == TRUE) {
-    ggplot2::theme_minimal() +
+    theme_obj +
       ggplot2::theme(
-        # Main elements
-        text = element_text(family = text_family,
-                            size = base_size,
-                            colour = text_colour),
-        title = element_text(family = title_family,
-                             colour = text_colour),
-        line = element_line(colour = line_colour),
-        panel.grid.major = element_line(colour = grid_colour),
-        panel.grid.minor = element_blank(),
-        
+        text = element_text(size = base_size),
+
         # Plot titles
-        plot.title = ggtext::element_markdown(face = "bold",
+        plot.title = ggtext::element_markdown(family = title_family,
+                                              face = "bold",
                                               size = base_size * 2,
                                               lineheight = 1.2,
                                               hjust = 0),
-        plot.title.position = "plot",
         plot.subtitle = ggtext::element_markdown(size = base_size * 1.4,
+                                                 lineheight = 1,
                                                  family = subtitle_family,
-                                                 hjust = 0,
-                                                 lineheight = 1), 
+                                                 hjust = 0), 
         # Plot margin & caption
-        plot.margin = plot_margin,
-        plot.caption.position = "plot", 
         plot.caption = ggtext::element_markdown(hjust = 0, 
                                                 colour = line_colour, 
                                                 margin = margin(t = 25, b = 25, l = 0, r = 0)),
-        # Background
-        plot.background = element_rect(fill = bg_colour,
-                                       colour = bg_colour),
-        
+
         # Axes
-        axis.text = element_text(size = base_size * 1.25, colour = text_colour,
-                                 margin = margin(t = 20, b = 20, l = 20, r = 20)),
+        axis.text = ggtext::element_markdown(size = base_size * 1.25, 
+                                             colour = text_colour,
+                                             margin = margin(t = 25, b = 25, l = 25, r = 25)),
         axis.title = ggtext::element_markdown(size = base_size * 1.6,
                                               hjust = 1,
                                               face = "italic",
@@ -139,115 +174,67 @@ theme_mark = function(title_family = "Cabin Condensed",
         axis.title.y.left = ggtext::element_markdown(size = base_size * 1.6,
                                                      hjust = 1,
                                                      face = "italic",
-                                                     margin = margin(r = 10, l = 10)),
+                                                     margin = margin(t = 0, r = 10, l = 10)),
         axis.title.y.right = ggtext::element_markdown(size = base_size * 1.6,
                                                       hjust = 0,
                                                       face = "italic",
-                                                      margin = margin(l = 15)),
-        axis.line = element_line(colour = line_colour),
-        
+                                                      margin = margin(t = 10, r = 10, l = 10)),
+
         # Legend
-        legend.title = ggtext::element_markdown(size = base_size * 1.2),
-        legend.text = ggtext::element_markdown(size = base_size * .9),
+        legend.title = ggtext::element_markdown(size = base_size * 1.3),
+        legend.text = ggtext::element_markdown(size = base_size * 1.1),
         
         # Facets
-        strip.text.y.left = element_text(size = base_size * 1.1, 
-                                         angle = 0, 
-                                         vjust = 0.5, 
-                                         hjust = 1),
-        strip.placement = "outside",
-        strip.text.y.right = element_text(size = base_size * 1.1, 
-                                          angle = 0, 
-                                          vjust = 0.5, 
-                                          hjust = 0),
+        strip.text.y.left = element_text(size = base_size * 1.1),
+        strip.text.y.right = element_text(size = base_size * 1.1)
         
       )
   } else if (plots_pane == TRUE & md == TRUE) {
-    ggplot2::theme_minimal(base_size = base_size) +
+    theme_obj +
       ggplot2::theme(
-        # Main elements
-        text = element_text(family = text_family,
-                            colour = text_colour),
-        title = element_text(family = title_family),
-        line = element_line(colour = line_colour),
-        panel.grid.major = element_line(colour = grid_colour),
-        panel.grid.minor = element_blank(),
-        
+
         # Plot titles
         plot.title = ggtext::element_markdown(face = "bold",
-                                              lineheight = 1.2),
-        plot.title.position = "plot",
-        plot.subtitle = ggtext::element_markdown(lineheight = 1.2),
+                                              lineheight = 1.2,
+                                              hjust = 0),
+        plot.subtitle = ggtext::element_markdown(lineheight = 1,
+                                                 family = subtitle_family,
+                                                 hjust = 0),
         
         # Plot margin and caption
-        plot.margin = plot_margin,
-        plot.caption.position = "plot", 
         plot.caption = ggtext::element_markdown(hjust = 0, 
                                                 colour = line_colour),
         
-        # Background
-        plot.background = element_rect(fill = bg_colour,
-                                       colour = bg_colour),
-        # panel.grid.major = element_line(colour = line_colour),
-        
+
         # Axes
         axis.title = ggtext::element_markdown(hjust = 1, 
                                               colour = text_colour,
                                               family = text_family,
-                                              face = "italic"),
-        axis.title.y.left = ggtext::element_markdown(margin = margin(r = 15), 
-                                                     colour = text_colour, 
+                                              face = "italic",
+                                              margin = margin(t = 10, b = 10, l = 10, r = 10)),
+        axis.title.y.left = ggtext::element_markdown(colour = text_colour, 
                                                      family = text_family,
-                                                     face = "italic"),
+                                                     face = "italic",
+                                                     margin = margin(t = 10, r = 10, l = 10)),
         axis.title.y.right = ggtext::element_markdown(hjust = 0, 
-                                                      margin = margin(l = 15), 
                                                       colour = text_colour, 
                                                       family = text_family,
-                                                      face = "italic"),
+                                                      face = "italic",
+                                                      margin = margin(t = 10, r = 10, l = 10)),
         axis.text = ggtext::element_markdown(colour = text_colour,
-                                             family = text_family),
-        axis.line = element_line(colour = line_colour),
-        
+                                             family = text_family,
+                                             margin = margin(5,5,5,5)),
+
         # Legend
         legend.title = ggtext::element_markdown(family = title_family),
         legend.text = ggtext::element_markdown(family = text_family)
+        
+        
       )
   } else {
-    ggplot2::theme_minimal(base_size = base_size) +
-      ggplot2::theme(
-        # Main elements
-        text = element_text(family = text_family,
-                            colour = text_colour),
-        title = element_text(family = title_family),
-        line = element_line(colour = line_colour),
-        panel.grid.major = element_line(colour = grid_colour),
-        panel.grid.minor = element_blank(),
-        
-        # Plot titles
-        plot.title = element_text(face = "bold",
-                                  lineheight = 1.2),
-        plot.title.position = "plot",
-        plot.subtitle = element_text(lineheight = 1.2),
-        # Plot margin and caption
-        plot.margin = plot_margin,
-        plot.caption.position = "plot", 
-        plot.caption = element_text(hjust = 0, 
-                                    colour = line_colour),
-        
-        # Background
-        plot.background = element_rect(fill = bg_colour,
-                                       colour = bg_colour),
-        
-        # Axes
-        axis.title = element_text(hjust = 1,
-                                  face = "italic"),
-        axis.title.y.left = element_text(hjust = 1,
-                                         face = "italic",
-                                         margin = margin(r = 15)),
-        axis.title.y.right = element_text(hjust = 0, 
-                                          face = "italic",
-                                          margin = margin(l = 15)),
-        axis.line = element_line(colour = line_colour)
-      )
+    # plots_pane == True & md == False
+    theme_obj
+
   }
 }
+
